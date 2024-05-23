ad2d2010862a74b1a0fce0f12cc99fbce0c9f806
Gangadhas <98011693+Gangadhas@users.noreply.github.com>
2024-05-23 21:35:27 +0800

    lambda

--- lambda.py
+++ lambda.py
@@ -1,3 +1,15 @@
+ num=[4,5,6,7,8,9]
+# n=[2,3,4,5,6,7]
+# list(map(lambda x:x+2))
+# A=add(num,n)
+# print(A)
+# # list(map(lambda x: 1 / (1 + math.exp(-x)), nums))
+# nums=[5,6]
+# A=lambda x:1/(1+x)
+# c=A(5)
+# print(c)
+
+
 num=[1,2,3,4,5,6,7,8,9]
 A=list(filter(lambda x:x%3,num))
 print(A)
